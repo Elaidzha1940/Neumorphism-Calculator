c38ddbd7288018df0de2c5a36bc2734b1d700d87
Elaidzha1940 <eli080293@gmail.com>
2023-06-04 09:55:06 +0700

    Neumorphism Calculator

---
+++ Neumorphism Calculator/Neumorphism_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neumorphism_CalculatorApp.swift
+//  Neumorphism Calculator
+//
+//  Created by Elaidzha Shchukin on 04.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Neumorphism_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
